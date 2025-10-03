56d720be4e74f46f2365f5b90ad109ab8c5ad244
korka-dev <diallo30amadoukorka@gmail.com>
2025-10-03 18:25:08 +0000

    feat: update config file

--- app/config.py
+++ app/config.py
@@ -3,19 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
-    postgres_url: str
     mongo_url: str
     access_token_expire_minutes: int = 1440 
     secret_key: str
     algorithm: str = "HS256"
     cors_origin:str="*"
     chunk_size: int = 1024 * 1024  
-    stripe_secret_key: str = ""
-    stripe_webhook_secret: str = ""
   
-    @property
-    def postgres_database_url(self) -> str:
-        return self.postgres_url
 
     @property
     def mongo_database_url(self) -> str:
